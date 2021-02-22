8099dd1391e9aa61884fec3b8663a3d3290adbe9
Dmitriy Filimonov <banodannon@gmail.com>
2021-02-22 17:16:24 +0300

    bool var is deleted

--- listCheckbox.js
+++ listCheckbox.js
@@ -1,34 +1,31 @@
-var expanded = false;
 
 function changeCheckboxDisplay(checkboxes){
-    if (!expanded) {
-        checkboxes.style.display = "block";
-        expanded = true;
-      } else {
-        checkboxes.style.display = "none";
-        expanded = false;
-      }
+  if (checkboxes.style.display=="none") {
+      checkboxes.style.display = "block";
+  } else {
+    checkboxes.style.display = "none";
+  }
 }
 
 function showCourses() {
-    var checkboxes = document.getElementById("courses");
-    changeCheckboxDisplay(checkboxes);
-  }
+  var checkboxes = document.getElementById("courses");
+  changeCheckboxDisplay(checkboxes);
+}
 
-  function showGroups() {
-    var checkboxes = document.getElementById("groups");
-    changeCheckboxDisplay(checkboxes);
-  }
+function showGroups() {
+  var checkboxes = document.getElementById("groups");
+  changeCheckboxDisplay(checkboxes);
+}
 
-  function showThemes() {
-    var checkboxes = document.getElementById("themes");
-    changeCheckboxDisplay(checkboxes);
-  }
+function showThemes() {
+  var checkboxes = document.getElementById("themes");
+  changeCheckboxDisplay(checkboxes);
+}
 
 let tags = ["variables", "arrays", "css", "cycles", "C#", "data structures", "linked list"];
 
 function cycle(){
-    for (i=0; i<tags.length; i++){
-        document.write(`<option value="`,tags[i],`"/>`);
-    }
+  for (i=0; i<tags.length; i++){
+      document.write(`<option value="`,tags[i],`"/>`);
+  }
 }
