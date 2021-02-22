d57f8229c8284ea317a15109dcea978c9c932122
Dmitriy Filimonov <banodannon@gmail.com>
2021-02-22 20:58:52 +0300

    need to fix height

--- listCheckbox.js
+++ listCheckbox.js
@@ -22,10 +22,23 @@ function showThemes() {
   changeCheckboxDisplay(checkboxes);
 }
 
+//tags
+
 let tags = ["variables", "arrays", "css", "cycles", "C#", "data structures", "linked list"];
 
 function cycle(){
   for (i=0; i<tags.length; i++){
       document.write(`<option value="`,tags[i],`"/>`);
+  }
+}
+
+//table cells customization
+
+let table = document.getElementsByClassName("table-row");
+
+for (i=0; i<table.length; i++){
+  let tableCell = table[i].getElementsByClassName("col");
+  for (j=0; j<tableCell.length; j++){
+    tableCell[j].style.border = "1px solid #0000FF";
   }
 }
